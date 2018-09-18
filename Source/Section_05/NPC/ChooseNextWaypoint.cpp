c48fe9e3ff7ce62ea2a2e99cf54842bd1034dad6
Tweon\Tweon-PC <sculpting@yahoo.com>
2018-09-19 01:29:42 +0700

    18_CPP Refactor_TidyingUP

---
+++ Source/Section_05/NPC/ChooseNextWaypoint.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NPC/ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrolRoute.h"
+
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+
+
+	
+	//Get Patrol Route
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+
+
+	//Protection against no Patrol Route component
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	//Warn against empty Patrol Routes
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0) 
+	{ 
+		UE_LOG(LogTemp, Warning, TEXT("Empty / Missing PatrolPoints!")); 
+		return EBTNodeResult::Failed;
+	}
+
+
+	//Set Next Waypoint
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+	
+	//Cycle The Index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+	
+	return EBTNodeResult::Succeeded;
+}
+
--- Source/Section_05/Section_05GameMode.cpp
+++ Source/Section_05/Section_05GameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "Section_05GameMode.h"
 #include "Section_05HUD.h"
-#include "Section_05Character.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ASection_05GameMode::ASection_05GameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
