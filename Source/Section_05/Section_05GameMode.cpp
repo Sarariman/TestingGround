822c45f2fe203a1649ccff0a3bc563adb6bf9c8e
Tweon\Tweon-PC <sculpting@yahoo.com>
2018-09-30 03:36:17 +0700

    34_Refactoring Folder_Fix Hit-Overlap Shooting Bug

--- Source/Section_05/Section_05GameMode.cpp
+++ Source/Section_05/Section_05GameMode.cpp
@@ -9,7 +9,7 @@ ASection_05GameMode::ASection_05GameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Character/Behaviour/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
